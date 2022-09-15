dee2dc2de08867f92086ae43b988efb5de0d053e
clover <104181288+ThreeLeafClover9@users.noreply.github.com>
2022-09-16 00:27:21 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -5,12 +5,8 @@ public class Solution {
         int answer = 0;
         while (n > 0) {
             answer += n % 10;
-            n = n / 10;
+            n /= 10;
         }
-
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println(answer);
-
         return answer;
     }
 }
