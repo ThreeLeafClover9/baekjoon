e91b2a74ebc20b7270220cb7812c862ecc93f284
clover <104181288+ThreeLeafClover9@users.noreply.github.com>
2022-09-18 20:56:24 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 1.08 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,10 +1,13 @@
+import java.util.*;
+
 class Solution {
     public String solution(String[] seoul) {
-        for (int i = 0; i < seoul.length; i++){
-            if (seoul[i].equals("Kim")) {
-                return String.format("김서방은 %d에 있다", i);
-            }
-        }
-        return "";
+        return String.format("김서방은 %d에 있다", Arrays.asList(seoul).indexOf("Kim"));
+        // for (int i = 0; i < seoul.length; i++){
+        //     if (seoul[i].equals("Kim")) {
+        //         return String.format("김서방은 %d에 있다", i);
+        //     }
+        // }
+        // return "";
     }
 }
