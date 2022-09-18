f2f0eb729e2888b29fc4187544a2803777c821fa
clover <104181288+ThreeLeafClover9@users.noreply.github.com>
2022-09-18 21:11:11 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 1.66 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -2,6 +2,6 @@ class Solution {
     public String solution(String phone_number) {
         int len = phone_number.length();
         String str = "*".repeat(len - 4);
-        return str += String.copyValueOf(phone_number.toCharArray(), len - 4, 4);
+        return str += phone_number.substring(len - 4, len);
     }
 }
