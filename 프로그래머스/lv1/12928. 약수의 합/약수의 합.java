99e6d1b737f767275e833af5086b5ac7eacde0b9
clover <104181288+ThreeLeafClover9@users.noreply.github.com>
2022-09-18 16:06:56 +0900

    [level 1] Title: 약수의 합, Time: 0.06 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
@@ -1,13 +1,9 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        
         for (int i = 1; i <= n; i++) {
-            int divisor = 0;
-            if (n % i == 0) divisor = i;
-            answer += divisor;
+            if (n % i == 0) answer += i;
         }        
-        
         return answer;
     }
 }
