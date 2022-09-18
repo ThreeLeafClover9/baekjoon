4435947b16f665d2ce518e1e003442b8b1b25153
clover <104181288+ThreeLeafClover9@users.noreply.github.com>
2022-09-18 21:38:09 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.05 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int[] arr) {
+        if (arr.length == 1) return new int[]{-1};
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < arr[index]) index = i;
+        }
+        int[] answer = new int[arr.length - 1];
+        for (int i = 0, j = 0; i < arr.length; i++) {
+            if (i != index) {
+                answer[j] = arr[i];
+                j++;
+            }
+        }
+        return answer;
+    }
+}
