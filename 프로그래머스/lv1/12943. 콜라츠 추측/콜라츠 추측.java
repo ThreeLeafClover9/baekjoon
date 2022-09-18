4e89666bec62b163cb46a692dbb0964acd91ba63
clover <104181288+ThreeLeafClover9@users.noreply.github.com>
2022-09-18 19:37:21 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,6 +1,15 @@
 class Solution {
     public int solution(int num) {
-        return solution2(num, 0);
+        long longNum = num;
+        int index = 0;
+        while (index < 500) {
+            if (longNum == 1) return index;
+            else if (longNum % 2 == 0) longNum /= 2;
+            else longNum = longNum * 3 + 1;
+            index++;
+        }
+        return -1;
+        // return solution2(num, 0);
     }
     
     public int solution2(long num, int count) {
