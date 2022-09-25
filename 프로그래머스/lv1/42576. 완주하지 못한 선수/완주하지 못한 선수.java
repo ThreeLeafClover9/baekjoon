9a41bf09d924c12ef86e5049ab836bdaa8bd0014
clover <104181288+ThreeLeafClover9@users.noreply.github.com>
2022-09-25 18:14:45 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 304.41 ms, Memory: 95.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,10 +4,11 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         Arrays.sort(participant);
         Arrays.sort(completion);
-        for (int i = 0; i < participant.length; i++) {
-            if (i == participant.length - 1) return participant[i];
+        int i = 0;
+        while (i < completion.length) {
             if (!participant[i].equals(completion[i])) return participant[i];
+            i++;
         }
-        return "";
+        return participant[i];
     }
 }
