89d2c88655994b8128afce879f6564dfcec400a5
clover <104181288+ThreeLeafClover9@users.noreply.github.com>
2022-09-26 00:12:51 +0900

    [Bronze V] Title: 시험 성적, Time: 208 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+    	Scanner scan = new Scanner(System.in);
+    	int a = scan.nextInt();
+    	char result;
+    	if (a >= 90) {
+    		result = 'A';
+    	} else if (a >= 80) {
+    		result = 'B';
+    	} else if (a >= 70) {
+    		result = 'C';
+    	} else if (a >= 60) {
+    		result = 'D';
+    	} else {
+    		result = 'F';
+    	}
+    	System.out.println(result);
+    	scan.close();		
+	}
+}
