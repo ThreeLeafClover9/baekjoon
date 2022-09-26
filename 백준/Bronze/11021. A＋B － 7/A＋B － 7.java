8c5a3dae01339777846f2802f4c7ede2ee041751
clover <104181288+ThreeLeafClover9@users.noreply.github.com>
2022-09-26 14:37:53 +0900

    [Bronze V] Title: A+B - 7, Time: 148 ms, Memory: 14692 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int next = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= next; i++) {
+            String s = br.readLine();
+            st = new StringTokenizer(s);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            String format = String.format("Case #%d: %d\n", i, a + b);
+            bw.write(format);
+        }
+        bw.close();
+    }
+}
