e381f16e11d1ba0efe880473ec800b3fc4861e7e
clover <104181288+ThreeLeafClover9@users.noreply.github.com>
2022-09-26 14:32:34 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 900 ms, Memory: 237524 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int next = Integer.parseInt(br.readLine());
+        for (int i = 0; i < next; i++) {
+            String s = br.readLine();
+            StringTokenizer st = new StringTokenizer(s);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write(a + b + "\n");
+        }
+        bw.close();
+    }
+}
