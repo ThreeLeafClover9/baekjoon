803e31d7a1d89607b05b17bd8bbabc00ff742dd5
clover <104181288+ThreeLeafClover9@users.noreply.github.com>
2022-09-26 15:43:24 +0900

    [Bronze III] Title: 최소, 최대, Time: 480 ms, Memory: 88012 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,23 +1,21 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int max = -1000000;
-		int min = 1000000;
-		
-		int t = Integer.parseInt(br.readLine());
-		String input = br.readLine();
-		StringTokenizer st = new StringTokenizer(input);
-		
-		for (int i = 0; i < t; ++i) {
-			int a = Integer.parseInt(st.nextToken());
-			max = Math.max(max, a);
-			min = Math.min(min, a);
-			}			
-		System.out.printf("%d %d",min, max);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int next = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        st = new StringTokenizer(str);
+        int min = Integer.parseInt(st.nextToken()), max = min;
+        for (int i = 1; i < next; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            min = Math.min(num, min);
+            max = Math.max(num, max);
+        }
+        System.out.println(min + " " + max);
+    }
 }
