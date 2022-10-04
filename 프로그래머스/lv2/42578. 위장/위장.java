dc776fd1dcdafba816e4ebe81771f508a6b14bd3
clover <104181288+ThreeLeafClover9@users.noreply.github.com>
2022-10-04 23:31:54 +0900

    [level 2] Title: 위장, Time: 0.05 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 위장/위장.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String[] clothe : clothes) {
+            String s = clothe[1];
+            if (map.containsKey(s)) {
+                Integer integer = map.get(s);
+                integer++;
+                map.put(s, integer);
+            } else map.put(s, 2);
+        }
+        int result = 1;
+        Collection<Integer> values = map.values();
+        for (Integer value : values) {
+            result *= value;
+        }
+        return result - 1;
+    }
+}
