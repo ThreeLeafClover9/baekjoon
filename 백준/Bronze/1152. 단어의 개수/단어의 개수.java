deffabd5850cb7edd5573d1a666e825bc8cebbc4
clover <104181288+ThreeLeafClover9@users.noreply.github.com>
2022-10-10 21:39:01 +0900

    [Bronze II] Title: 단어의 개수, Time: 248 ms, Memory: 19988 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,13 +1,12 @@
+import java.util.*;
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
         String str = br.readLine();
-        st = new StringTokenizer(str);
+        StringTokenizer st = new StringTokenizer(str);
         System.out.println(st.countTokens());
     }
 }
