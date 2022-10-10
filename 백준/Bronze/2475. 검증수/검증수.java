56c37f0037fdb58017b021d46e4f3bc4a527992f
clover <104181288+ThreeLeafClover9@users.noreply.github.com>
2022-10-10 22:29:32 +0900

    [Bronze V] Title: 검증수, Time: 120 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,21 @@
+import com.sun.security.jgss.GSSUtil;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        StringTokenizer st = new StringTokenizer(str);
+        int next = st.countTokens();
+        int sum = 0;
+        for (int i = 0; i < next; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            sum += num * num;
+        }
+
+        System.out.println(sum % 10);
+    }
+}
