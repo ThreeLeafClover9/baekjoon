df1d9d267734227b43e935db0ca9d5a8d3b54a3b
clover <104181288+ThreeLeafClover9@users.noreply.github.com>
2022-10-10 22:58:45 +0900

    [Bronze II] Title: 문자열 반복, Time: 116 ms, Memory: 14080 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -1,21 +1,22 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int next = Integer.parseInt(br.readLine());
         for (int i = 0; i < next; i++) {
-            String[] split = br.readLine().split(" ");
-            int repeat = Integer.parseInt(split[0]);
-            String str = split[1];
-            String result = "";
-            for (int j = 0; j < str.length(); j++) {
-                String s = String.valueOf(str.charAt(j));
-                result += s.repeat(repeat);
+            String str = br.readLine();
+            StringTokenizer st = new StringTokenizer(str);
+            int repeat = Integer.parseInt(st.nextToken());
+            String s = st.nextToken();
+            for (int j = 0; j < s.length(); j++) {
+                String ch = String.valueOf(s.charAt(j));
+                bw.write(ch.repeat(repeat));
             }
-            bw.write(result + "\n");
+            bw.write("\n");
         }
         bw.close();
     }
