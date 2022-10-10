c6e5e29cecfcfb3a2084dcb3bd27ff6e9654e16b
clover <104181288+ThreeLeafClover9@users.noreply.github.com>
2022-10-10 22:24:51 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 128 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -1,15 +1,14 @@
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int next = Integer.parseInt(br.readLine());
+
         for (int i = 1; i <= next; i++) {
-            String str = " ".repeat(next - i);
-            str += "*".repeat(i);
-            bw.write(str + "\n");
+            bw.write(" ".repeat(next - i));
+            bw.write("*".repeat(i) + "\n");
         }
         bw.close();
     }
