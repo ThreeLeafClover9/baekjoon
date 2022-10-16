7eec1ac63a82496cb97491e0dfa66d28ecff56f0
clover <104181288+ThreeLeafClover9@users.noreply.github.com>
2022-10-16 22:07:41 +0900

    [Silver IV] Title: 수 찾기, Time: 556 ms, Memory: 44132 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,5 +1,5 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -7,17 +7,19 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int next = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str);
         Set<String> set = new HashSet<>();
         for (int i = 0; i < next; i++) {
             set.add(st.nextToken());
         }
-
         next = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
+        str = br.readLine();
+        st = new StringTokenizer(str);
         for (int i = 0; i < next; i++) {
-            if (set.contains(st.nextToken())) bw.write(1 + "\n");
-            else bw.write(0 + "\n");
+            String s = st.nextToken();
+            if (set.contains(s)) bw.write("1\n");
+            else bw.write("0\n");
         }
         bw.close();
     }
