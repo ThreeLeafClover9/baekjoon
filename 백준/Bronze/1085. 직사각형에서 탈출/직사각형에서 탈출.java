e46246ed0e3d11839055aef8539e1f43dfef7778
clover <104181288+ThreeLeafClover9@users.noreply.github.com>
2022-10-17 15:56:41 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 124 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str);
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+        System.out.println(Math.min(Math.min(w - x, x), Math.min(h - y, y)));
+    }
+}
