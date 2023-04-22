7bc9f25c6de6a4bab9ddcb2ea060e98354852aa4
clover <104181288+ThreeLeafClover9@users.noreply.github.com>
2023-04-22 22:32:14 +0900

    [Silver II] Title: 색종이 만들기, Time: 160 ms, Memory: 16232 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -12,30 +12,25 @@ public class Main {
             String str = br.readLine();
             matrix[i] = str.split(" ");
         }
-        System.out.println(logic(new int[]{0, 0}, new int[]{next, next}, matrix, "0"));
-        System.out.println(logic(new int[]{0, 0}, new int[]{next, next}, matrix, "1"));
+        System.out.println(logic(0, 0, next, matrix, "0"));
+        System.out.println(logic(0, 0, next, matrix, "1"));
     }
 
-    private static int logic(int[] start, int[] end, String[][] matrix, String value) {
+    private static int logic(int row, int col, int len, String[][] matrix, String value) {
+        if (len == 1) return matrix[row][col].equals(value) ? 1 : 0;
         int count = 0;
-        boolean check = true;
-        if (start[0] == end[0] || start[1] == end[1]) return 0;
-        outer:
-        for (int i = start[0]; i < end[0]; i++) {
-            for (int j = start[1]; j < end[1]; j++) {
+        for (int i = row; i < row + len; i++) {
+            for (int j = col; j < col + len; j++) {
                 if (!matrix[i][j].equals(value)) {
-                    check = false;
-                    count += logic(new int[]{start[0], start[1]}, new int[]{(end[0] + start[0]) / 2, (end[1] + start[1]) / 2}, matrix, value);
-                    count += logic(new int[]{start[0], (end[1] + start[1]) / 2}, new int[]{(end[0] + start[0]) / 2, end[1]}, matrix, value);
-                    count += logic(new int[]{(end[0] + start[0]) / 2, start[1]}, new int[]{end[0], (end[1] + start[1]) / 2}, matrix, value);
-                    if (end[0] - start[0] == 1) {
-                        count += logic(end, end, matrix, value);
-                    } else count += logic(new int[]{(end[0] + start[0]) / 2, (end[1] + start[1]) / 2}, new int[]{end[0], end[1]}, matrix, value);
-                    break outer;
+                    int half = len / 2;
+                    count += logic(row, col, half, matrix, value);
+                    count += logic(row, col + half, half, matrix, value);
+                    count += logic(row + half, col, half, matrix, value);
+                    count += logic(row + half, col + half, half, matrix, value);
+                    return count;
                 }
             }
         }
-        if (check) count++;
-        return count;
+        return ++count;
     }
 }
