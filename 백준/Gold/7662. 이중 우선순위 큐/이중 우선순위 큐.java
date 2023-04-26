23d9023a968e3d6025f535a9663ffc4e05df93c3
clover <104181288+ThreeLeafClover9@users.noreply.github.com>
2023-04-26 17:28:09 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2308 ms, Memory: 360196 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -1,9 +1,8 @@
-import com.sun.source.tree.Tree;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -13,15 +12,15 @@ public class Main {
         for (int i = 0; i < T; i++) {
             int k = Integer.parseInt(br.readLine());
             TreeMap<Integer, Integer> map = new TreeMap<>();
-//            int count = 0;
             for (int j = 0; j < k; j++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 String operator = st.nextToken();
                 int num = Integer.parseInt(st.nextToken());
                 logic(map, operator, num);
             }
-            if (map.size() > 0) sb.append(map.lastKey()).append(" ").append(map.firstKey()).append("\n");
-            else sb.append("EMPTY").append("\n");
+            if (map.size() > 0) sb.append(map.lastKey()).append(" ").append(map.firstKey());
+            else sb.append("EMPTY");
+            sb.append("\n");
         }
         System.out.println(sb);
     }
@@ -32,20 +31,10 @@ public class Main {
             else map.put(num, 1);
         } else {
             if (map.size() > 0) {
-                if (num == 1) {
-                    Integer key = map.lastKey();
-                    Integer value = map.get(key);
-                    if (value > 1) {
-                        map.put(key, value - 1);
-                    } else map.remove(key);
-                }
-                else {
-                    Integer key = map.firstKey();
-                    Integer value = map.get(key);
-                    if (value > 1) {
-                        map.put(key, value - 1);
-                    } else map.remove(key);
-                }
+                int key = num == 1 ? map.lastKey() : map.firstKey();
+                int value = map.get(key);
+                if (value > 1) map.put(key, value - 1);
+                else map.remove(key);
             }
         }
     }
