4f7c5e0dc68f291d42166e06b8fb8bc64f7bd34a
clover <104181288+ThreeLeafClover9@users.noreply.github.com>
2023-04-28 15:46:50 +0900

    [Silver III] Title: Four Squares, Time: 152 ms, Memory: 14436 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int squares = squares(N);
+        System.out.println(squares);
+    }
+
+    private static int squares(int n) {
+        int[] arr = new int[n + 1];
+//        arr[1] = 1;
+        for (int i = 1; i <= n; i++) {
+            int num = arr[i - 1];
+            for (int j = 2; j * j <= i; j++) {
+                int tmp = arr[i - j * j];
+                num = Math.min(num, tmp);
+            }
+            arr[i] = num + 1;
+        }
+        return arr[n];
+    }
+}
