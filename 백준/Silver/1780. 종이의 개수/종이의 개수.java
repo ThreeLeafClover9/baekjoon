211d9db92972f291f34681e82de4234f1cb396fb
clover <104181288+ThreeLeafClover9@users.noreply.github.com>
2023-04-28 22:26:49 +0900

    [Silver II] Title: 종이의 개수, Time: 1380 ms, Memory: 487180 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.java
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.java
@@ -25,15 +25,11 @@ public class Main {
     private static void numberOfPaper(int row, int col, String[][] matrix, int len, Map<String , Integer> map) {
         if (!check(row, col, matrix, len, map)) {
             int newLen = len / 3;
-            numberOfPaper(row, col, matrix, newLen, map);
-            numberOfPaper(row, col + newLen, matrix, newLen, map);
-            numberOfPaper(row, col + newLen * 2, matrix, newLen, map);
-            numberOfPaper(row + newLen, col, matrix, newLen, map);
-            numberOfPaper(row + newLen, col + newLen , matrix, newLen, map);
-            numberOfPaper(row + newLen, col + newLen * 2, matrix, newLen, map);
-            numberOfPaper(row + newLen * 2, col, matrix, newLen, map);
-            numberOfPaper(row + newLen * 2, col + newLen, matrix, newLen, map);
-            numberOfPaper(row + newLen * 2, col + newLen * 2, matrix, newLen, map);
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    numberOfPaper(row + newLen * i, col + newLen * j, matrix, newLen, map);
+                }
+            }
         }
     }
 
