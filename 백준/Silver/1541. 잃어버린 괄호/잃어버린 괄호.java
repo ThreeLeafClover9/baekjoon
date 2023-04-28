6d7fa898d5d88f6c20819712bf7eaf04acf12b5e
clover <104181288+ThreeLeafClover9@users.noreply.github.com>
2023-04-28 21:49:48 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 124 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,37 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        String num = "";
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-            if (c == '+') {
-                list.add(Integer.parseInt(num));
-                num = "";
-            } else if (c == '-') {
-                list.add(Integer.parseInt(num));
-                num = "-";
-            } else num += c;
-        }
-        list.add(Integer.parseInt(num));
-        int bracket = bracket(list);
+        String[] split = br.readLine().split("-");
+        int bracket = bracket(split);
         System.out.println(bracket);
     }
 
-    private static int bracket(List<Integer> list) {
+    private static int bracket(String[] split) {
         int result = 0;
-        boolean plus = true;
-        for (int i = 0; i < list.size(); i++) {
-            Integer num = list.get(i);
-            if (num < 0) plus = false;
-            if (plus) result += Math.abs(num);
-            else result -= Math.abs(num);
+        for (int i = 0; i < split.length; i++) {
+            String[] str = split[i].split("\\+");
+            for (String s : str) {
+                if (i == 0) result += Integer.parseInt(s);
+                else result -= Integer.parseInt(s);
+            }
         }
         return result;
     }
