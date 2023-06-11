c5c4311ef1240f76bd93e4fb1d47ed87f8f08a4b
clover <104181288+ThreeLeafClover9@users.noreply.github.com>
2023-06-11 16:18:49 +0900

    [Gold II] Title: 보석 도둑, Time: 1752 ms, Memory: 120200 KB -BaekjoonHub

---
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[][] base = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int m = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            base[i][0] = m;
+            base[i][1] = v;
+        }
+        int[] bag = new int[k];
+        for (int i = 0; i < k; i++) {
+            bag[i] = Integer.parseInt(br.readLine());
+        }
+        long max = max(base, bag, n, k);
+        System.out.println(max);
+    }
+
+    private static long max(int[][] base, int[] bag, int n, int k) {
+        Arrays.sort(base, Comparator.comparingInt(o -> o[0]));
+        Arrays.sort(bag);
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.reverseOrder());
+        long result = 0;
+        for (int i = 0, j = 0; i < k; i++) {
+            while (j < n && base[j][0] <= bag[i]) {
+                queue.offer(base[j++][1]);
+            }
+            if (!queue.isEmpty()) {
+                result += queue.poll();
+            }
+        }
+        return result;
+    }
+}
