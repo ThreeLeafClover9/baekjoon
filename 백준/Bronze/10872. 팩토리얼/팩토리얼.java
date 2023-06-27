983d02a31f940e86251c403dd18938a4b8e7423a
clover <104181288+ThreeLeafClover9@users.noreply.github.com>
2023-06-27 23:45:35 +0900

    [Bronze V] Title: 팩토리얼, Time: 132 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int factorial = factorial(n);
+        System.out.println(factorial);
+    }
+
+    private static int factorial(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
